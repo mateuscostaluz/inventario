74f21116eaf0527a00e56e6ab2e06b97b98a6066
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-04-17 12:52:43 -0300

    add: test authorization to item endpoints

--- test/userController.test.js
+++ test/userController.test.js
@@ -3,7 +3,6 @@ import app from '../src/server'
 import Database from '../src/database/index'
 
 describe('Test Users endpoints', () => {
-
   test('Should save an user', async () => {
     const response = await request(app)
       .post('/users')
@@ -22,6 +21,8 @@ describe('Test Users endpoints', () => {
   })
 
   test('Should list all users', async () => {
+    await Database.connection.models.User.truncate()
+
     await request(app)
       .post('/users')
       .send({ name: 'Usuario 1', email: 'user3@email.com', password: 'password' })
@@ -39,7 +40,7 @@ describe('Test Users endpoints', () => {
       .get('/users')
       .set('Authorization', 'bearer ' + token)
     const { body } = response
-    expect(body.length).toBe(4)
+    expect(body.length).toBe(2)
     expect(response.statusCode).toBe(200)
   })
 })
