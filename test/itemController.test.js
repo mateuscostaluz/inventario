b01efb31678ea7e52313ad104b09cce6b98440d0
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-04-22 12:11:24 -0300

    Testes de validação de item

--- test/itemController.test.js
+++ test/itemController.test.js
@@ -8,6 +8,7 @@ import Department from '../src/app/models/Department'
 describe('Test Items endpoints', () => {
   let token
   let depId
+  let itemId
 
   beforeAll(async () => {
     await request(app)
@@ -21,9 +22,10 @@ describe('Test Items endpoints', () => {
     token = authResponse.body.token
 
     const { id } = await Department.create({ name: 'Depart01' })
-
     depId = id
-    console.log(depId)
+
+    const { id: iId } = await Item.create({ name: 'Cadeira', department_id: depId })
+    itemId = iId
   })
 
   test('Should save an Item', async () => {
@@ -34,18 +36,25 @@ describe('Test Items endpoints', () => {
     expect(response.statusCode).toBe(201)
   })
 
-  test('Should response bad request error', async () => {
+  test('Should response bad request error for name validation', async () => {
     const response = await request(app)
       .post('/item')
       .set('Authorization', 'bearer ' + token)
-      .send({ nome: 'Cadeira' })
+      .send({ name: 1234, department_id: depId })
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('Should response bad request error for department id validation', async () => {
+    const response = await request(app)
+      .post('/item')
+      .set('Authorization', 'bearer ' + token)
+      .send({ name: 'Mesa', department_id: 'valor' })
     expect(response.statusCode).toBe(400)
   })
 
   test('Should update an item', async () => {
-    const { id } = await Item.create({ name: 'Cadeira', department_id: depId })
     const response = await request(app)
-      .put('/item/' + id)
+      .put('/item/' + itemId)
       .set('Authorization', 'bearer ' + token)
       .send({ name: 'Mesa', department_id: depId })
     expect(response.statusCode).toBe(200)
@@ -57,6 +66,22 @@ describe('Test Items endpoints', () => {
       .put('/item/' + id)
       .set('Authorization', 'bearer ' + token)
       .send({ name: 'Mesa' })
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('Should response bad request error for name validation on update', async () => {
+    const response = await request(app)
+      .put('/item/' + itemId)
+      .set('Authorization', 'bearer ' + token)
+      .send({ name: 14 })
+    expect(response.statusCode).toBe(400)
+  })
+
+  test('Should response bad request error for department id validation on update', async () => {
+    const response = await request(app)
+      .put('/item/' + itemId)
+      .set('Authorization', 'bearer ' + token)
+      .send({ department_id: 'id' })
     expect(response.statusCode).toBe(400)
   })
 
