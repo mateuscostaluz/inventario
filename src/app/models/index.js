4676575a9e23e92edccb5a7f9a27df436070b6f5
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-04-27 12:47:13 -0300

    Implementação de função adicional truncate

---
+++ src/app/models/index.js
@@ -0,0 +1,3 @@
+import models from './'
+
+export default models
---
+++ test/util/models.js
@@ -0,0 +1,9 @@
+import Item from '../src/app/models/Item'
+import ItemInventory from '../../src/app/models/ItemInventory'
+import Inventory from '../../src/app/models/Inventory'
+import User from '../../src/app/models/User'
+import Department from '../../src/app/models/Department'
+
+export default {
+  Item, ItemInventory, Inventory, User, Department
+}
