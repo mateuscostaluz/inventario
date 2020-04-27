e16bd33494f4115799f67b7d4c8378b6f99d8e44
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-04-27 12:49:16 -0300

    Arquivo models em testes apagado

--- test/util/models.js
+++
@@ -1,9 +0,0 @@
-import Item from '../src/app/models/Item'
-import ItemInventory from '../../src/app/models/ItemInventory'
-import Inventory from '../../src/app/models/Inventory'
-import User from '../../src/app/models/User'
-import Department from '../../src/app/models/Department'
-
-export default {
-  Item, ItemInventory, Inventory, User, Department
-}
